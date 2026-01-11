259717fe5f4953371890bf37b9fee6b43be393d4
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:50:31 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "app_service_certificate_orders" {
+  description = <<EOT
+Map of app_service_certificate_orders, attributes below
+Required:
+    - location
+    - name
+    - resource_group_name
+Optional:
+    - auto_renew
+    - csr
+    - distinguished_name
+    - key_size
+    - product_type
+    - tags
+    - validity_in_years
+EOT
+
+  type = map(object({
+    location            = string
+    name                = string
+    resource_group_name = string
+    auto_renew          = optional(bool, true)
+    csr                 = optional(string)
+    distinguished_name  = optional(string)
+    key_size            = optional(number, 2048)
+    product_type        = optional(string, "Standard")
+    tags                = optional(map(string))
+    validity_in_years   = optional(number, 1)
+  }))
+}
+
